1ffbf867c700c97ba015ac8d854a3ade28b308e3
ROHAN <ROHAN@ROHAN-PC>
2016-05-20 17:12:59 +0530

    Doing it second time ..in Doopp

---
+++ JavaBoys/src/good/Doop.java
@@ -0,0 +1,13 @@
+package good;
+
+public class Doop {
+
+	public void ain()
+	{
+		System.out.println("doing operation in so so ...");
+		
+		System.out.println("Doing .......It second time i Doop folder");
+		
+	}
+	
+}
